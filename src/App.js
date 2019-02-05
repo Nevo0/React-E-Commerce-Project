54b4fce2b547b682ee887a96860f1ca316454865
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-05 12:01:11 +0100

    Creat app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          {/* musimy dodac exact zeby aplikacja dzialala poprawnie*/}
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          {/* jezeli nie ma takiego linku zostanie wyswietlone to co jest w Default component */}
+          <Route component={Default} />
+        </Switch>
+        {/* <ProductList />
+        <Details />
+        <Cart />
+        <Default /> */}
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>Hellow form navbar</h3>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
