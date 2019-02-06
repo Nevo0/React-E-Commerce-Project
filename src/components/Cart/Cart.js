00f76312272ed99b93f6bda223e9a70368a8e19f
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-06 21:57:47 +0100

    Start creat cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Title from "../Title";
+import CartColums from "./CartColums";
+import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from "../../context";
+
+class Cart extends Component {
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColums />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
+}
+
+export default Cart;
